1652a30830b7db6764221bf3993fc303c3519bc8
iLapaa <emanuel_lapa@hotmail.com>
2023-08-22 22:01:15 +0100

    sign up page

--- website/src/app/sign-up/[[...sign-up]]/page.tsx
+++ website/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <SignUp />
+      </div>
+    </>
+  );
 }
