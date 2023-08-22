a2d125de3da9954c997b56e5ed52adfd65661cc8
iLapaa <emanuel_lapa@hotmail.com>
2023-08-22 22:01:08 +0100

    sign in page

--- website/src/app/sign-in/[[...sign-in]]/page.tsx
+++ website/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <SignIn />
+      </div>
+    </>
+  );
 }
