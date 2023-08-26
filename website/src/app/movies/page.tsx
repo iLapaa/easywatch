e2dba886b268d909e806f53d1e8a1de269c0a98c
iLapaa <emanuel_lapa@hotmail.com>
2023-08-26 12:15:12 +0100

    movies page displaying movies

--- website/src/app/movies/page.tsx
+++ website/src/app/movies/page.tsx
@@ -1,9 +1,22 @@
 import DisplayMovies from "@/components/display-movies";
+import prisma from "@/lib/prisma";
+import getMovies from "../api/getMovies";
+import MovieCard from "@/components/movie-card";
 
-export default function Home() {
+export default async function Home() {
+  const movie = await getMovies();
+
   return (
-    <main className="mt-4 flex gap-4">
+    <main className="mt-4 grid grid-cols-7 gap-4">
       <DisplayMovies />
+
+      {movie.map((mov, index) => (
+        <MovieCard
+          key={index}
+          movieUrl={"movies/title/" + mov.imdbId}
+          title={mov.title}
+        />
+      ))}
     </main>
   );
 }
